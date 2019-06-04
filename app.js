8fdda5d8bc5cb113324c70c9e136e8c4ce9133db
tzl <tzl@weiyian.com>
2019-06-04 23:34:47 +0800

    update

--- app.js
+++ app.js
@@ -2,25 +2,24 @@ import { getItem } from './utils/util'
 import {getApplicationList, getDomainList, getDomainOutDto, getMainClassList, getSenior} from "./utils/services";
 
 App({
-    onLaunch: function() {
-        let hd_token = getItem('hd_token') || '';
-        let hd_userId = getItem('hd_userId') || '';
-        // 检测本地用户信息
-        if( !hd_token || !hd_userId){
-            wx.reLaunch({
-                url: '/pages/other/login/login'
-            })
-        }else{
-            wx.reLaunch({
-                url: '/pages/main/index/index'
-            })
-        }
-    },
-    onShow (options) {
-        getApplicationList()
-        getDomainList();
-        getDomainOutDto();
-        getMainClassList();
-        getSenior();
-    },
+	onLaunch: function() {
+		let hd_token = getItem('hd_token') || '';
+		let hd_userId = getItem('hd_userId') || '';
+		getApplicationList()
+		getDomainList();
+		getDomainOutDto();
+		getMainClassList();
+		getSenior();
+		// 检测本地用户信息
+		if( !hd_token || !hd_userId){
+			wx.reLaunch({
+				url: '/pages/other/login/login'
+			})
+		}else{
+			wx.reLaunch({
+				url: '/pages/main/index/index'
+			})
+		}
+	},
+
 })
